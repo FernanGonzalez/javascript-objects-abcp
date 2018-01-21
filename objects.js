97cc974ca8f678f8d439cc045c4e6b93d63c31f5
Fernán González Domingo <fernangonzalez@gmail.com>
2018-01-21 13:56:51 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,1 +1,5 @@
 var playlist = { artistName: "The Cranberries", songTitle: "Zombie"};
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist.artistName=artistName;
+  playlist.songTitle=songTitle;
+}
