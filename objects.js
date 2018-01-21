a21fe7e1a70e501df6f407e32d6896c9941d3ae8
Fernán González Domingo <fernangonzalez@gmail.com>
2018-01-21 14:08:40 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,10 @@
 var playlist = {'The Cranberries': 'Zombie'}
 
-function updatePlaylist(playlist, artist, song){
-  playlist[artist] = song
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle
   return playlist
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist.artistName;
 }
